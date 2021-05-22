3009e82b2447a359f066ff850fd36cc2a7aed2cb
Robin Drexler <drexler.robin@gmail.com>
2021-05-22 16:22:46 -0400

    initial setup

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import typescript from "@rollup/plugin-typescript";
+
+import externals from "rollup-plugin-node-externals";
+
+const name = "last-promise";
+
+export default {
+  input: "src/index.ts",
+  plugins: [
+    // `declarationDir` seems to be relative to the the output dir.
+    // not changing it to ./ causes d.ts files to end up in a ./dist/dist folder.
+    typescript({ tsconfig: "./tsconfig.json", declarationDir: "./" }),
+    externals({ deps: true }),
+  ],
+  output: [
+    {
+      file: `dist/${name}.js`,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: `dist/${name}.mjs`,
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("lel");
+
+export function foo(name: string) {
+  console.log(name);
+}
